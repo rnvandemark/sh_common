f8f84cfba252c2d6cb36cd20b4ab9568d3554ea0
R. Nick Vandemark <rnvandemark@gmail.com>
2021-12-29 02:44:02 -0500

    Add constant, shared names of topics and services.

--- include/sh_common/ros_names.hpp
+++ include/sh_common/ros_names.hpp
@@ -12,6 +12,39 @@ namespace sh { namespace names {
     public:
         DECL(HEARTBEAT_PERIOD_MS);
     };
+
+    class topics {
+    public:
+        // Heartbeat
+        DECL(HEARTBEAT);
+        // Mode change
+        DECL(REQUESTED_MODE_CHANGES);
+        DECL(CONFIRMED_MODE_CHANGES);
+        // Color peak calculation
+        DECL(LEFT_COLOR_PEAK);
+        DECL(RIGHT_COLOR_PEAK);
+        DECL(COLOR_PEAKS_TELEM);
+        // Sound file playback
+        DECL(REQUESTED_PLAYBACK_FILES);
+        DECL(PLAYBACK_COMMANDS);
+        DECL(PLAYBACK_UPDATES);
+        DECL(PLAYBACK_FREQUENCIES);
+        // Individual control mode
+        DECL(INTENSITY_CHANGE_UPDATES);
+        // Countdown mode
+        DECL(COUNTDOWN_STATE_UPDATES);
+        // Wave mode
+        DECL(START_WAVE_MODE);
+        DECL(WAVE_PARTICIPANT_LOCATION);
+        DECL(WAVE_UPDATES);
+    };
+
+    class services {
+    public:
+        // Screen color coordinator (scc)
+        DECL(REQUEST_SCREEN_COLOR_CALIBRTION);
+        DECL(SET_SCREEN_COLOR_HOMOG_POINTS);
+    };
 }}
 
 #undef DECL
@@ -32,8 +65,30 @@ namespace {
     }
 }
 
-#define DEFI(name, components) const std::string sh::names::name = acc(components)
+#define DEFI(name, value) const std::string sh::names::name = value
 
-DEFI(params::HEARTBEAT_PERIOD_MS, {"heartbeat_period_ms"});
+// Parameters
+DEFI(params::HEARTBEAT_PERIOD_MS, acc({"heartbeat_period_ms"}));
+
+// Topics
+DEFI(topics::HEARTBEAT, acc({"heartbeat"}));
+DEFI(topics::REQUESTED_MODE_CHANGES, acc({"mode_change", "requested"}));
+DEFI(topics::CONFIRMED_MODE_CHANGES, acc({"mode_change", "confirmed"}));
+DEFI(topics::LEFT_COLOR_PEAK, acc({"color_peaks", "left"}));
+DEFI(topics::RIGHT_COLOR_PEAK, acc({"color_peaks", "right"}));
+DEFI(topics::COLOR_PEAKS_TELEM, acc({"color_peaks", "telem"}));
+DEFI(topics::REQUESTED_PLAYBACK_FILES, acc({"sound_file_playback", "requested"}));
+DEFI(topics::PLAYBACK_COMMANDS, acc({"sound_file_playback", "commands"}));
+DEFI(topics::PLAYBACK_UPDATES, acc({"sound_file_playback", "telem"}));
+DEFI(topics::PLAYBACK_FREQUENCIES, acc({"sound_file_playback", "frequencies"}));
+DEFI(topics::INTENSITY_CHANGE_UPDATES, acc({"device_intensities"}));
+DEFI(topics::COUNTDOWN_STATE_UPDATES, acc({"countdown_state"}));
+DEFI(topics::START_WAVE_MODE, acc({"wave", "requests"}));
+DEFI(topics::WAVE_PARTICIPANT_LOCATION, acc({"wave", "participant_locations"}));
+DEFI(topics::WAVE_UPDATES, acc({"wave", "position"}));
+
+// Services
+DEFI(services::REQUEST_SCREEN_COLOR_CALIBRTION, acc({"screen_color_coordination", "request_calibration"}));
+DEFI(services::SET_SCREEN_COLOR_HOMOG_POINTS, acc({"screen_color_coordination", "set_homog_points"}));
 
 #undef DEFI
--- src/sh_common_py.cpp
+++ src/sh_common_py.cpp
@@ -11,4 +11,31 @@ BOOST_PYTHON_MODULE(sh_common_py) {
         bp::scope sc = bp::class_<sn::params>("params", bp::no_init);
         sc.attr("HEARTBEAT_PERIOD_MS") = sn::params::HEARTBEAT_PERIOD_MS;
     }
+
+    // Add topics
+    {
+        bp::scope sc = bp::class_<sn::topics>("topics", bp::no_init);
+        sc.attr("HEARTBEAT") = sn::topics::HEARTBEAT;
+        sc.attr("REQUESTED_MODE_CHANGES") = sn::topics::REQUESTED_MODE_CHANGES;
+        sc.attr("CONFIRMED_MODE_CHANGES") = sn::topics::CONFIRMED_MODE_CHANGES;
+        sc.attr("LEFT_COLOR_PEAK") = sn::topics::LEFT_COLOR_PEAK;
+        sc.attr("RIGHT_COLOR_PEAK") = sn::topics::RIGHT_COLOR_PEAK;
+        sc.attr("COLOR_PEAKS_TELEM") = sn::topics::COLOR_PEAKS_TELEM;
+        sc.attr("REQUESTED_PLAYBACK_FILES") = sn::topics::REQUESTED_PLAYBACK_FILES;
+        sc.attr("PLAYBACK_COMMANDS") = sn::topics::PLAYBACK_COMMANDS;
+        sc.attr("PLAYBACK_UPDATES") = sn::topics::PLAYBACK_UPDATES;
+        sc.attr("PLAYBACK_FREQUENCIES") = sn::topics::PLAYBACK_FREQUENCIES;
+        sc.attr("INTENSITY_CHANGE_UPDATES") = sn::topics::INTENSITY_CHANGE_UPDATES;
+        sc.attr("COUNTDOWN_STATE_UPDATES") = sn::topics::COUNTDOWN_STATE_UPDATES;
+        sc.attr("START_WAVE_MODE") = sn::topics::START_WAVE_MODE;
+        sc.attr("WAVE_PARTICIPANT_LOCATION") = sn::topics::WAVE_PARTICIPANT_LOCATION;
+        sc.attr("WAVE_UPDATES") = sn::topics::WAVE_UPDATES;
+    }
+
+    // Add services
+    {
+        bp::scope sc = bp::class_<sn::services>("services", bp::no_init);
+        sc.attr("REQUEST_SCREEN_COLOR_CALIBRTION") = sn::services::REQUEST_SCREEN_COLOR_CALIBRTION;
+        sc.attr("SET_SCREEN_COLOR_HOMOG_POINTS") = sn::services::SET_SCREEN_COLOR_HOMOG_POINTS;
+    }
 }
